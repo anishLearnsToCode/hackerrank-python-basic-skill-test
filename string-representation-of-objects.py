d8a6099d846593176811a6fb54b7dabdbe7dc3f9
anishLearnsToCode <anish_bt2k16@dtu.ac.in>
2020-07-14 11:20:34 +0530

    solves string representation of objects

--- string-representation-of-objects.py
+++ string-representation-of-objects.py
@@ -2,3 +2,18 @@ class Car:
     def __init__(self, max_speed, unit):
         self.max_speed = max_speed
         self.unit = unit
+
+    def __str__(self):
+        return 'Car with the maximum speed of ' + str(self.max_speed) + ' ' + str(self.unit)
+
+
+class Boat:
+    def __init__(self, max_speed):
+        self.max_speed = max_speed
+
+    def __str__(self):
+        return 'Boat with the maximum speed of ' + str(self.max_speed) + ' knots'
+
+
+car = Car(120, 'km/h')
+print(car)
